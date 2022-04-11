43257df549412f4a80770dd8301f14db07caa632
Can Razboyalı <canrazbonyali@gmail.com>
2022-04-11 10:43:51 +0300

    11.04.2022 tarihli ders kodları

---
+++ 11-12.04.2022/11.04.2022/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11._04._2022
+{
+
+    class Program
+    {
+        public struct Kisi
+        {
+            public string adSoyad;
+            public int ceptekiPara;
+
+            public Kisi(string d1, int d2)
+            {
+                this.adSoyad = d1;
+                this.ceptekiPara = d2;
+            }
+        };
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Kisi k1 = new Kisi();
+
+                Kisi k2 = new Kisi();
+
+                Kisi k3 = new Kisi();
+
+                Kisi k4 = new Kisi();
+
+                Kisi k5 = new Kisi();
+
+                Kisi ben = new Kisi("Can Razbonyalı", 200);
+
+
+                /*sinemaBiletKuyruk.Enqueue(k1);
+                sinemaBiletKuyruk.Enqueue(k2);
+                sinemaBiletKuyruk.Enqueue(k3);
+                sinemaBiletKuyruk.Enqueue(ben);
+                sinemaBiletKuyruk.Enqueue(k4);
+                sinemaBiletKuyruk.Enqueue(k5);*/
+
+
+                Kisi[] kisiler = { k1, k2, k3, ben, k4, k5 };
+                Queue<Kisi> sinemaBiletKuyruk = new Queue<Kisi>(kisiler);
+
+                while (sinemaBiletKuyruk.Count > 0)
+                {
+                    Kisi siradakiKisi = sinemaBiletKuyruk.Peek();
+                    if (siradakiKisi.adSoyad != null)
+                    {
+                        siradakiKisi.ceptekiPara -= 50;
+                        ben = siradakiKisi;
+                    }
+                    
+                    sinemaBiletKuyruk.Dequeue();
+                }
+
+                Console.WriteLine("Sira eridi - ", sinemaBiletKuyruk.Count);
+
+                //string[] kisiler = { "Cem", "Ali", "Cemil" };
+
+                /*
+                 ilk giren ilk çıkar - FIFO
+                 */
+                //kuyruk.Count
+
+                //Queue<string> kuyruk = new Queue<string>();
+                //Queue<string> kuyruk2 = new Queue<string>(kisiler);
+                //Queue<string> kuyruk3 = new Queue<string>();
+
+
+                /*Ekleme
+                kuyruk.Enqueue("Deneme");
+                kuyruk.Enqueue("Deneme2");
+                kuyruk.Enqueue("Deneme3");
+
+                Console.WriteLine(kuyruk.Contains("deneme2"));*/
+
+                /*Çıkartma*/
+                //Console.WriteLine("Çıkan eleman {0}", kuyruk2.Dequeue());
+
+                /*kuyruk3.Enqueue("Exception");
+                if (!(kuyruk3.Count > 0))
+                {
+                    throw new Exception("Kuyruk boş!");
+                }
+                Console.WriteLine("Çıkan eleman {0}", kuyruk3.Dequeue());*/
+
+                //kuyruk3.Enqueue(122);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata oldu - {0}", ex);
+            }
+
+        }
+    }
+}
---
+++ Hafta 6/05.04.2022/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _05._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 1, 3, 5, 6 };
+            List<int> liste = new List<int>(sayilar);
+
+
+            /*for (int i = 0; i < liste.Count; i++)
+            {
+                Console.Write(liste[i]);
+            }
+
+            Console.WriteLine("foreach");
+
+            foreach (var eleman in liste)
+            {
+                Console.Write(eleman + ",");
+            }*/
+
+            try
+            {
+                ArrayList arrList = new ArrayList();
+                //ArrayList arrList2 = new ArrayList();
+
+
+
+                arrList.Add(false);//cinsiyet
+                arrList.Add(100);
+                arrList.Add("Can Razbonyalı");//ad soyad
+                /*arrList.Add(5.2);
+                arrList.Add('c');*/
+
+                foreach (var satir in arrList)
+                {
+                    Console.Write(satir);
+                }
+
+                if (!(bool)arrList[0]) { //cast
+                    Exception ex = new Exception("deneme");
+                }
+
+                Console.WriteLine();
+                arrList[2] = Convert.ToInt32(arrList[2]) + 100;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Program çok başarılı " + ex);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+
+
+            //arrList2.Add(arrList);
+
+
+            /* Console.WriteLine("Liste içinde liste");
+
+             foreach (ArrayList elemanArrList2 in arrList2)
+             {
+                 foreach (var eleman in elemanArrList2)
+                 {
+                     Console.Write(eleman + ",");
+
+                 }
+             }*/
+
+
+        }
+    }
+}
